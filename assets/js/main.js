57ada37cf6c45521217eff429358699ddf73eb5d
Finnschy <skylerfearless@yahoo.de>
2020-11-11 21:05:26 +0100

    core game functions done

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,53 +28,126 @@ ich brauche eine funktion mit addeventlistener, click für die icons
 // console.log(radioValue);
 
 
-let radios = document.querySelectorAll("[type=radio]")
-for (let i = 0; i < radios.length; i++) {
-    console.log(radios[i].value);
-    }
+// let radios = document.querySelectorAll("[type=radio]")
+// for (let i = 0; i < radios.length; i++) {
+//     console.log(radios[i].value);
+//     }
 
-/* radio buttons: when checked = true dann aktion */ 
+/* radio buttons: when checked = true dann aktion */
 
 
 //--- Ab hier wirds ernst!!! ------------------------------------------------
 
+//--- Alle wichtigen Dinge aus dem HTML Dokument holen:
 let rock = document.getElementById("rock")
 let paper = document.getElementById("paper")
 let scissor = document.getElementById("scissor")
 let result = document.getElementById("result")
-console.log(rock);
+let userCount = document.getElementById("userCount")
+let compCount = document.getElementById("compCount")
+let icons = document.querySelectorAll("img")
+let userCounter = 0
+let compCounter = 0
 
 //------funktion für den zufallsgenerator
-function randomizer() {
+// function randomizer() {
+//     let choices = ['rock', 'paper', 'scissors'];
+//     let randomNumber = Math.floor(Math.random() * 3);
+//     return choices[randomNumber];
+//   }
+//   console.log(randomizer());
+
+//------obige funktion mal als arrowfunktion
+const randomizer2 = () => {
     let choices = ['rock', 'paper', 'scissors'];
     let randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
-  }
-  console.log(randomizer());
-//---------------------------------------
+}
 
+//---------------------------------------
 
 //------funktion für icon click
-function iconEventClick() {
-    rock.addEventListener("click", () => {
-        console.log(rock.id);
-        result.innerHTML = rock.id
-    })
-    
-    paper.addEventListener("click", () => {
-        console.log(paper.id);
-        result.innerHTML = paper.id
-    })
-    
-    scissor.addEventListener("click", () => {
-        console.log(scissor.id);
-        result.innerHTML = scissor.id
-    })
-}
-iconEventClick()
+rock.addEventListener("click", event => {
+    let compChoice = randomizer2()
+    switch (compChoice) {
+        case "rock":
+            console.log("rock");
+            result.innerHTML = `${rock.id}(User) can't beat ${compChoice}(Comp). Draw! :|`
+            rock.classList.add("draw")
+            setTimeout(() => rock.classList.remove('draw'), 700)
+            break
+
+        case "scissors":
+            console.log("scissors");
+            result.innerHTML = `${rock.id}(User) beats ${compChoice}(Comp). Win! :)`
+            userCounter++
+            userCount.innerHTML = userCounter
+            rock.classList.add("win")
+            setTimeout(() => rock.classList.remove('win'), 700)
+            break
+
+        case "paper":
+            console.log("paper");
+            result.innerHTML = `${rock.id}(User) gets beaten by ${compChoice}(Comp). Lose! :(`
+            compCounter++
+            compCount.innerHTML = compCounter
+            rock.classList.add("lose")
+            setTimeout(() => rock.classList.remove('lose'), 700)
+            break
+    }
+})
+
+paper.addEventListener("click", event => {
+    let compChoice = randomizer2()
+    switch (compChoice) {
+        case "rock":
+            result.innerHTML = `${paper.id}(User) beats ${compChoice}(Comp). Win! :)`
+            userCounter++
+            userCount.innerHTML = userCounter
+            paper.classList.add("win")
+            setTimeout(() => paper.classList.remove('win'), 700)
+            break
+
+        case "scissors":
+            result.innerHTML = `${paper.id}(User) gets beaten by ${compChoice}(Comp). Lose! :(`
+            compCounter++
+            compCount.innerHTML = compCounter
+            paper.classList.add("lose")
+            setTimeout(() => paper.classList.remove('lose'), 700)
+            break
+
+        case "paper":
+            result.innerHTML = `${paper.id}(User) can't beat ${compChoice}(Comp). Draw! :|`
+            paper.classList.add("draw")
+            setTimeout(() => paper.classList.remove('draw'), 700)
+            break
+    }
+})
+
+scissor.addEventListener("click", event => {
+    let compChoice = randomizer2()
+    switch (compChoice) {
+        case "rock":
+            result.innerHTML = `${scissor.id}(User) gets beaten by ${compChoice}(Comp). Lose! :(`
+            compCounter++
+            compCount.innerHTML = compCounter
+            scissor.classList.add("lose")
+            setTimeout(() => scissor.classList.remove('lose'), 700)
+            break
+
+        case "scissors":
+            result.innerHTML = `${scissor.id}(User) can't beat ${compChoice}(Comp). Draw! :|`
+            scissor.classList.add("draw")
+            setTimeout(() => scissor.classList.remove('draw'), 700)
+            break
+
+        case "paper":
+            result.innerHTML = `${scissor.id}(User) beats ${compChoice}(Comp). Win! :)`
+            userCounter++
+            userCount.innerHTML = userCounter
+            scissor.classList.add("win")
+            setTimeout(() => scissor.classList.remove('win'), 700)
+            break
+    }
+})
 //---------------------------------------
-
-
-//------funktion zum vergleich
-
-
